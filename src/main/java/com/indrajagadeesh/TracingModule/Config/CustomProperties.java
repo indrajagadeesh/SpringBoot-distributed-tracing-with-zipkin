ef09225b31fce6646bd220e7918ac2581c9f132a
indrajagadeesh <indrajagadeesh121@gmail.com>
2020-08-09 18:57:38 +0530

    add properties

--- src/main/java/com/indrajagadeesh/TracingModule/Config/CustomProperties.java
+++ src/main/java/com/indrajagadeesh/TracingModule/Config/CustomProperties.java
@@ -11,9 +11,9 @@ import lombok.Setter;
 @Getter
 @Setter
 public class CustomProperties {
-    private boolean nextCall;
-    private String hostName;
-    private String port;  
+    private boolean nextCall = false;
+    private String hostName = "localhost";
+    private String port = "8080";
     private long delay = 0;
     private String message = "This is a test message";
 }
