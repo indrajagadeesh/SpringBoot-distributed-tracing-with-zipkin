4a55dc0accd828fe1f0127fe68adc00776b45bfb
indrajagadeesh <indrajagadeesh121@gmail.com>
2020-08-09 19:02:40 +0530

    fix

--- src/test/java/com/indrajagadeesh/TracingModule/TracingModuleApplicationTests.java
+++ src/test/java/com/indrajagadeesh/TracingModule/TracingModuleApplicationTests.java
@@ -1,13 +1,14 @@
-package com.indrajagadeesh.TracingModule;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class TracingModuleApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.indrajagadeesh.TracingModule;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//
+//class TracingModuleApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
