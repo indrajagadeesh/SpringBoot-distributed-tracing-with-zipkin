8944df9c9a0f7a5d6a016fd7c396fc3a2f05e3f0
indrajagadeesh <indrajagadeesh121@gmail.com>
2020-08-11 10:17:50 +0530

    updated trace sampling rate to always and added zipkin in docker compose file

--- src/main/java/com/indrajagadeesh/TracingModule/Config/ApplicationConfig.java
+++ src/main/java/com/indrajagadeesh/TracingModule/Config/ApplicationConfig.java
@@ -3,6 +3,7 @@ package com.indrajagadeesh.TracingModule.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import brave.sampler.Sampler;
 
 @Configuration
 public class ApplicationConfig {
@@ -10,5 +11,10 @@ public class ApplicationConfig {
     @Bean
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
+	}
+
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
 	}
 }
